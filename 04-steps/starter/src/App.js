9738f7207933a6b044b635b2ce33d4e5a30c83c0
Marcos García <marcos.g.costell@outlook.com>
2025-08-22 00:49:46 +0200

    6-setting-state-manually

--- 04-steps/starter/src/App.js
+++ 04-steps/starter/src/App.js
@@ -7,6 +7,7 @@ const messages = [
 ];
 export default function App() {
   const [step, setStep] = useState(1);
+  const [test, setTest] = useState({ name: "Marcos" });
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -14,6 +15,10 @@ export default function App() {
 
   function handleNext() {
     if (step < 3) setStep(step + 1);
+
+    // BAD PRACTICE
+    // test.name = "Fred";
+    setTest({ name: "Fred" });
   }
 
   return (
@@ -26,6 +31,7 @@ export default function App() {
 
       <p className="message">
         Step {step}: {messages[step - 1]}
+        {test.name}
       </p>
 
       <div className="buttons">
